6aaba52d3bad97e168062134ea320fd841f7f4d2
Mckreames <114367512+Mckreames@users.noreply.github.com>
2024-02-24 10:56:12 -0500

    spacing and news sect edits

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,15 +5,15 @@ import "./NavBar.css";
 
 export default function NavBar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-      <div class="container-fluid">
-        <a class="navbar-brand offset-1 me-auto" href="#top">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+      <div className="container-fluid">
+        <a className="navbar-brand offset-1 me-auto" href="#top">
           <img src={logo} className="App-logo" alt="logo" />
           CurrentVue
         </a>
         <form role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
     </nav>
--- src/components/NewsSect/NewsSect.jsx
+++ src/components/NewsSect/NewsSect.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from 'react';
+import "./NewsSect.css";
 
 export default function NewsSect() {
   return (
     <section>
         <div>
-            <h2 className="d-flex justify-content-center news-head">News Near You</h2>
+            <h2 className="d-flex justify-content-center mt-5 news-head">News Near You</h2>
         </div>
-        <div className="row mt-5">
-            <div className="story story1 col-3 offset-1"></div>
-            <div className="story story2 col-4"></div>
-            <div className="story story3 col-3"></div>
+        <div className="row mt-4 d-flex justify-content-center">
+            <div className="story story1 col-3 offset-1">
+              <h3 className="text-align-center news-head">Lorem ipsum dolor sit amet.</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolorum corrupti ea qui explicabo aspernatur ipsum vero. Quo, ipsam rerum deleniti quisquam quaerat tempore officiis.</p>
+            </div>
+            <div className="story story2 col-4">
+            <h3 className="text-align-center news-head">Lorem ipsum dolor sit amet.</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolorum corrupti ea qui explicabo aspernatur ipsum vero. Quo, ipsam rerum deleniti quisquam quaerat tempore officiis.</p>
+            </div>
+            <div className="story story3 col-3">
+            <h3 className="text-align-center news-head">Lorem ipsum dolor sit amet.</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolorum corrupti ea qui explicabo aspernatur ipsum vero. Quo, ipsam rerum deleniti quisquam quaerat tempore officiis.</p>
+            </div>
         </div>
     </section>
   )
