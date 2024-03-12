0a28ddc82ec6d2c1d88d6871ebeb0f0ab091012a
Mckreames <114367512+Mckreames@users.noreply.github.com>
2024-03-11 21:00:24 -0400

    added news stories

--- src/components/NewsSect/NewsSect.jsx
+++ src/components/NewsSect/NewsSect.jsx
@@ -6,7 +6,7 @@ export default function NewsSect() {
   return (
     <section className="background-color">
         <div>
-            <h2 className="d-flex justify-content-center pt-5 news-header">News Around The World</h2>
+            <h2 className="d-flex justify-content-center pt-5 news-header">Breaking News Around The World</h2>
         </div>
         <div className="col-10 col-lg-12 row mt-4 mx-auto pb-5 d-flex flex-column flex-lg-row justify-content-center story-container">
             <div className="story story1 h-100 mx-auto mb-5 col col-md-10 col-lg-3 offset-0 offset-lg-1 text-center">
@@ -20,6 +20,20 @@ export default function NewsSect() {
             <div className="story story3 h-100 mx-auto mb-5 col col-md-10 col-lg-3 text-center">
                 <h3 className="news-head"><a href="https://www.theonion.com/amazon-fires-employee-who-tested-positive-for-having-fo-1851049319" target="_blank">Amazon Fires Employee Who Tested Positive For Having Food In Their System</a></h3>
                 <p className="d">AMAZON-Amazon has fired an employee who tested positive for having food in their system. The employee violated Amazon's strict, zero-tolerance policy against the use of food by their employees. This comes after she failed a routine, random food test. Amazon reminds employees, "Do not come to work under the influence of nourishment. Showing up to work fed can jeoporadize the safe starvation of their co-workers"</p>
+            </div>
+        </div>
+        <div className="col-10 col-lg-12 row mt-4 mx-auto pb-5 d-flex flex-column flex-lg-row justify-content-center story-container">
+            <div className="story story1 h-100 mx-auto mb-5 col col-md-10 col-lg-3 offset-0 offset-lg-1 text-center">
+                <h3 className="news-head"><a href="https://www.theonion.com/waitstaff-watches-helplessly-as-limo-full-of-screaming-1851303980" target="_blank">Waitstaff Watches Helplessly As Limo Full Of Screaming 12-Year-Old Girls Pulls Up To Restaurant</a></h3>
+                <p className="d">WILLOW GLEN, CA—Knowing full well that their fate had been sealed, local waitstaff at TGI Fridays watched helplessly Thursday as a limousine full of screaming 12-year-old girls pulled up in front of their restaurant. “Dear God, we’re doomed,” said head waiter James Orsen, who stood motionless as several preteens dressed in matching shirts sprinted past him and immediately piled into two separate booths where they began laughing, yelling, and knocking over various pieces of glassware. That middle-aged woman they came in with just sat at a separate table and ordered wine. She knows what’s up. At press time, an exhausted looking Orsen informed reporters he would be taking the rest of the night off after one of the giggling 12-year-olds told him he was cute and asked if he was on Snapchat.</p>
+            </div>
+            <div className="story story2 h-100 mx-auto mb-5 col col-md-10 col-lg-4 text-center">
+                <h3 className="news-head"><a href="https://www.theonion.com/zelensky-challenges-putin-to-settle-ukraine-war-on-the-1851325870" target="_blank">Zelensky Challenges Putin To Settle Ukraine War On The Dance Floor</a></h3>
+                <p className="d"><a href="https://www.theonion.com/zelensky-challenges-putin-to-settle-ukraine-war-on-the-1851325870" target="_blank">VIDEO</a></p>
+            </div>
+            <div className="story story3 h-100 mx-auto mb-5 col col-md-10 col-lg-3 text-center">
+                <h3 className="news-head"><a href="https://www.theonion.com/secret-service-finds-biden-attempting-to-dig-own-grave-1851305493" target="_blank">Secret Service Finds Biden Attempting To Dig Own Grave On White House Lawn</a></h3>
+                <p className="d"><a href="https://www.theonion.com/secret-service-finds-biden-attempting-to-dig-own-grave-1851305493">VIDEO</a></p>
             </div>
         </div>
     </section>
